25b9bcff74c319b4839e457069e6b79207bfc029
Santiago-Gallego <santiagoxd68@hotmail.com>
2020-05-01 01:18:04 +0000

    [Add]

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    count = len(sys.argv)
+    from sys import argv
+    count = len(argv)
     if count == 1:
-        print("{:d} argument.".format(count - 1))
+        print("0 arguments.")
     elif count == 2:
-        print("{:d} argument:".format(count - 1))
+        print("1 argument:")
+        print("{0}: {1}".format(1, argv[1]))
     else:
-        print("{:d} arguments:".format(count - 1))
-    for i in range(count):
-        if i == 0:
-            continue
-        print("{:d}: {:s}".format(i, sys.argv[i]))
+        print("{} arguments:".format(count - 1))
+        for i in range(1, count):
+            print("{0}: {1}".format(i, argv[i]))
