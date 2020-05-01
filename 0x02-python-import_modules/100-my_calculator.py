6b211ffca864881a9b0302af1e08fabbad4081f1
Santiago-Gallego <santiagoxd68@hotmail.com>
2020-05-01 01:20:07 +0000

    [Add]

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,11 +7,9 @@ if __name__ == "__main__":
     if arg_len != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-
     a = int(sys.argv[1])
     b = int(sys.argv[3])
     op = sys.argv[2]
-
     if op == '+' :
         print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
     elif op == "-":
