23123d475f3b8f2ba6f7b1a75ec8a6cb0f92fb9f
Santiago-Gallego <santiagoxd68@hotmail.com>
2020-05-05 19:21:17 +0000

    [Add]

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,94 +1,31 @@
-#include "lists.h"
+ #include "lists.h"
 
 /**
- * _strlen_recursion - returns length of a string
- * @s: string to be assessed
- * Return: length of string
+ * is_palindrome - function to call check_pal to see if list is palindrome
+ * @head: ptr to the beginning of the list
+ * Return: 0 if not palindrome else 1
  */
-
-int _strlen_recursion(char *s)
-{
-	if (*s == '\0')
-		return (0);
-	return (_strlen_recursion(s + 1) + 1);
-}
-
-/**
- * check_palin - check if string is a palindrome
- * @s: string to be assessed
- * @start: first index
- * @end: end index
- * Return: 1 if palindrome, 0 if not palindrome
- */
-
-int check_palin(char *s, int start, int end)
-{
-	if (s[start] != s[end])
-		return (0);
-	if (start >= end)
-		return (1);
-	return (check_palin(s, start + 1, end - 1));
-}
-
-/**
- * is_palin - determine if string is a palindrome
- * @s: string to be assessed
- * Return: 1 if string is palindrome and 0 if not
- */
-
-int is_palin(char *s)
-{
-	int end;
-
-	if (*s == '\0')
-		return (1);
-	end = _strlen_recursion(s) - 1;
-	return (check_palin(s, 0, end));
-}
-
-/**
- * is_palindrome - check if a singly linked list is a palindrome
- *
- * @head: start of linked list
- * Return: 0 if it is not a palindrome, 1 if it is a palindrome
- */
-
 int is_palindrome(listint_t **head)
 {
-	listint_t *temp = NULL;
-	listint_t *p = NULL;
-	char *s;
-	int numNodes = 0;
-	int i = 0;
-
-	if (*head == NULL || head == NULL)
+	if (head == NULL || *head == NULL)
 		return (1);
+	return (check_pal(head, *head));
+}
 
-	p = *head;
-	while (p != NULL)
+/**
+ * check_pal - function to check if the list is palindrome
+ * @head: ptr to the beginning of the list
+ * @last: ptr to the end of the list
+ * Return: 0 if not palindrom else 1
+ */
+int check_pal(listint_t **head, listint_t *last)
+{
+	if (last == NULL)
+		return (1);
+	if (check_pal(head, last->next) && (*head)->n == last->n)
 	{
-		p = p->next;
-		numNodes++;
-	}
-
-	s = malloc(sizeof(int) * numNodes);
-	if (s == NULL)
-		return (-1);
-
-	temp = *head;
-
-	while (temp != NULL)
-	{
-		s[i] = temp->n;
-		temp = temp->next;
-		i++;
-	}
-
-	if (is_palin(s) == 1)
-	{
-		free(s);
+		*head = (*head)->next;
 		return (1);
 	}
-	free(s);
 	return (0);
 }
