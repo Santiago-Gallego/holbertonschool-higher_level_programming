b9be34c54ff62f858c6316d529554361dad3485b
Santiago-Gallego <santiagoxd68@hotmail.com>
2020-05-07 21:19:32 +0000

    [Fix] error syntax

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     no = list(set(my_list))
+    res = 0
     for i in range(len(no)):
-        result += no[i]
-    return result
+        res += no[i]
+    return res
