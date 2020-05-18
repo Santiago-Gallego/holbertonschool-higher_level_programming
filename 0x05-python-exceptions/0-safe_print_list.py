a447f9a0aa9026220aae8d7581c19c372693c584
Santiago-Gallego <santiagoxd68@hotmail.com>
2020-05-18 17:30:17 +0000

    [add]

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,10 @@
+#!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     count = 0
     for item in my_list:
         if count in range(x):
             try:
-                print(item, end="")
+                print(item, end='')
                 count += 1
             except:
                 break
