094f223db289e36476474d5255f4d98986039782
Santiago-Gallego <santiagoxd68@hotmail.com>
2020-06-03 01:24:07 +0000

    [Fix error of the comparation]

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""Function that returns True if is instance of class """
-
-
+""" Function that verifies if the object"""
 def is_same_class(obj, a_class):
+    """Return true or false"""
     return (type(obj) == a_class)
