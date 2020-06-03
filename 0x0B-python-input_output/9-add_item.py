453ae84878e7e8e4da6cf5547bada948d84826dd
Santiago-Gallego <60366040+Santiago-Gallego@users.noreply.github.com>
2020-06-03 17:44:55 -0500

    Update 9-add_item.py

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-""" Load, Save """
+""" Project to use two early funcitons """
 from sys import argv
 
-load_json = __import__('8-load_from_json_file').load_from_json_file
-save_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 
 try:
-    li = load_json('add_item.json')
-    save_json_file(li + argv[1:], 'add_item.json')
+    li = load_from_json_file('add_item.json')
+    save_to_json_file(li + argv[1:], 'add_item.json')
 except Exception:
-    save_json_file(argv[1:], 'add_item.json')
+    save_to_json_file(argv[1:], 'add_item.json')
