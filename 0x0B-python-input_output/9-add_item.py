0a75a8d5f22d99f245864b30ba73829057a8a9d6
Santiago-Gallego <60366040+Santiago-Gallego@users.noreply.github.com>
2020-06-03 17:45:50 -0500

    Update 9-add_item.py

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-""" Project to use two early funcitons """
-from sys import argv
+"""Load, Add, Save"""
 
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+
+import sys
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_json = __import__('8-load_from_json_file').load_from_json_file
 
 try:
-    li = load_from_json_file('add_item.json')
-    save_to_json_file(li + argv[1:], 'add_item.json')
-except Exception:
-    save_to_json_file(argv[1:], 'add_item.json')
+    my_list = load_json("add_item.json")
+except:
+    my_list = []
+for i in range(1, len(sys.argv)):
+    my_list.append(sys.argv[i])
+save_to_json_file(my_list, "add_item.json")
