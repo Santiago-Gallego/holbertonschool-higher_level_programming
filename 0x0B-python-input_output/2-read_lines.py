68e4a3a04acbd0d276111581692f4f0b5a567669
Santiago-Gallego <60366040+Santiago-Gallego@users.noreply.github.com>
2020-06-03 17:21:20 -0500

    Update 2-read_lines.py

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -8,9 +8,9 @@ def read_lines(filename="", nb_lines=0):
     """ Read n lines of text file """
     line_count = 0
     with open(filename, 'r') as f:
-        if number_lines <= 0:
+        if nb_lines <= 0:
             print(f.read(), end='')
         for line in f:
-            if line_count < number_lines:
+            if line_count < nb_lines:
                 print(line, end='')
                 line_count += 1
