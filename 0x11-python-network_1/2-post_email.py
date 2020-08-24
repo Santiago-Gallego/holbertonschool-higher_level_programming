251fc5a2f6a0cadf0a7a091a498ea09425696000
Santiago-Gallego <santiagoxd68@hotmail.com>
2020-08-24 23:27:14 +0000

    [update] pycodestyle

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" Takes in a URL and an email, sends a POST request to the passed URL with the email """
+""" Takes in a URL and an email, sends a POST request to the
+passed URL with the email """
 if __name__ == "__main__":
     import urllib.request
     import urllib.parse
