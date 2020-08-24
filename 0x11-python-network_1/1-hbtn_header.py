3a150b7ba78a53df8941eb8a4b89fd1ee166addd
Santiago-Gallego <santiagoxd68@hotmail.com>
2020-08-24 23:24:22 +0000

    ]

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Takes in a URL, sends a request to the URL """
+
+
+if __name__ == "__main__":
+    import urllib.request
+    import sys
+
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        head = response.headers.get('X-Request-Id')
+        print(head)
