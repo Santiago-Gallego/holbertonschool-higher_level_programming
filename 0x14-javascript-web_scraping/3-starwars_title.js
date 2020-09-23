6e3bceba7c9b1daab0757b0e882a162dd69693bf
Santiago-Gallego <santiagoxd68@hotmail.com>
2020-09-23 02:26:37 +0000

    [Add]

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
+const URL = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 const request = require('request');
 
-request('http://swapi.co/api/films/' + process.argv[2], function (error, response, body) {
-  if (error) {
-    console.error(error);
-  }
-  console.log(JSON.parse(body).title);
+request(URL, function (err, response, body) {
+  const jsonObj = JSON.parse(body);
+  console.log(jsonObj.title);
+  if (err) {}
 });
